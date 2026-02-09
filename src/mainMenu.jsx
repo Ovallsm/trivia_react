8186346e72d78c3a8307e20ade4c83a7d2a75c89
Oscar-Valls <ovallsmoreno@gmail.com>
2026-02-09 11:51:08 +0100

    first commit

---
+++ src/mainMenu.jsx
@@ -0,0 +1,12 @@
+import "./mainMenu.css";
+
+export default function MainMenu() {
+  return (
+    <>
+      <div id="BtnContainer">
+        <button>Join game</button>
+        <button>Create game</button>
+      </div>
+    </>
+  );
+}
